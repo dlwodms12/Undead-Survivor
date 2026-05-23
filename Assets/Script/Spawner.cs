179eee0f142954fc53dd988ab36f54b84adcc51a
Jae Eun <pdw05498@naver.com>
2026-05-23 19:48:51 +0900

    스포너 기능 구현

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
 
+    public Transform[] spawnPoint;
+
+    float timer;
+
+    void Awake()
+    {
+        //Spawner의 본인 + 자식 컴포넌트(스폰 포인트)를 가져와서 배열에 할당
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        timer += Time.deltaTime;
+
+        if(timer > 0.2f)
         {
-            GameManager.instance.pool.Get(1);
+            Spawn();
+            timer = 0;
         }
+    }
+
+    void Spawn()
+    {
+        //적 오브젝트를 게임 매니저를 통해 가져와서 할당
+        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,2));
+        //랜덤한 스폰 포인트에 적 오브젝트 생성 (본인(Spawner)를 제외하고 자식 오브젝트(Point)에 생성)
+        enemy.transform.position = spawnPoint[Random.Range(1,spawnPoint.Length)].position;
     }
 }
