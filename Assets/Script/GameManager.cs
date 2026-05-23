a6a8ef217139fe8c316b7e7c117858ed686b2269
Jae Eun <pdw05498@naver.com>
2026-05-23 20:08:21 +0900

    소환 레벨 적용
    -시간이 지나면 레벨이 올라가면서 스폰되는 몬스터와 수가 달라지게끔 구현
    -SpawnData라는 몬스터 속성값을 직렬화

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,11 +6,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
+
     public PoolManager pool;
     public Player player;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
     }
 }
--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -6,7 +6,9 @@ public class Spawner : MonoBehaviour
 {
 
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
+    int level;
     float timer;
 
     void Awake()
@@ -19,8 +21,10 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        //시간에 흐름에 따라 레벨이 올라감
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
 
-        if(timer > 0.2f)
+        if(timer > (level == 0 ? 0.5f : 0.2f))
         {
             Spawn();
             timer = 0;
@@ -30,8 +34,19 @@ public class Spawner : MonoBehaviour
     void Spawn()
     {
         //적 오브젝트를 게임 매니저를 통해 가져와서 할당
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,2));
+        //레벨이 변하면 할당하는 오브젝트도 달라짐
+        GameObject enemy = GameManager.instance.pool.Get(level);
         //랜덤한 스폰 포인트에 적 오브젝트 생성 (본인(Spawner)를 제외하고 자식 오브젝트(Point)에 생성)
         enemy.transform.position = spawnPoint[Random.Range(1,spawnPoint.Length)].position;
     }
+}
+
+//직렬화
+[System.Serializable]
+public class SpawnData
+{
+    public int spriteType;
+    public float spawnTime;
+    public int health;
+    public float speed;
 }
