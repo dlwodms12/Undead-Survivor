3695715fc0f8400e6288b0a8bbabf2e1ab1caf18
Jae Eun <pdw05498@naver.com>
2026-05-23 14:38:18 +0900

    스포너 기능 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public PoolManager pool;
     public Player player;
 
     private void Awake()
