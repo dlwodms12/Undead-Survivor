c86e125975db563ed84a7c56d7f8fa67c3361fe1
Jae Eun <pdw05498@naver.com>
2026-05-28 09:34:49 +0900

    업적 관리 기능 추가
    -PlayerPrefs 를 통해 유저의 데이터를 받아와서 업적이 해금됐는지 확인

---
+++ Assets/Script/AchiveManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchiveManager : MonoBehaviour
+{
+    public GameObject[] lockCharacter;
+    public GameObject[] unlockCharacter;
+
+    //업적 종류
+    enum Achive { UnlockPotato, UnlockBean }
+
+    //업적 데이터를 저장
+    Achive[] achives;
+
+    //초기화
+    private void Awake()
+    {
+        //Enum.GetValues : 열거형의 모든 데이터를 가져오는 함수
+        achives = (Achive[])Enum.GetValues(typeof(Achive));
+
+        //PlayerPrefs에 MyData 값이 0이라면(게임을 처음 실행했다면)
+        if (!PlayerPrefs.HasKey("MyData"))
+        {
+            Init();
+        }
+    }
+
+    //데이터 초기화
+    void Init()
+    {
+        PlayerPrefs.SetInt("MyData", 1);
+
+        foreach (Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 0);
+        }
+    }
+
+    void Start()
+    {
+        UnlockCharacter();
+    }
+
+    //캐릭터 잠금해제
+    void UnlockCharacter()
+    {
+        //잠겨진 캐릭터들 순회
+        for (int index = 0; index < lockCharacter.Length; index++)
+        {
+            //업적 이름 받아오기
+            string achiveName = achives[index].ToString();
+            //플레이어 데이터에서 해당 업적이 1(해금됐는지) 확인하는 변수
+            bool isUnlock = PlayerPrefs.GetInt(achiveName) == 1;
+            //잠겨진 캐릭터의 업적이 해금됐다면 잠겨진 캐릭터 활성화
+            lockCharacter[index].SetActive(!isUnlock);
+            //업적이 잠겨있다면 해제된 캐릭터를 다시 잠금
+            unlockCharacter[index].SetActive(isUnlock);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
