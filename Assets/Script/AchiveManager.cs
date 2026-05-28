cb055d5b5bcb63e4a47886ffa23cf1995209f497
Jae Eun <pdw05498@naver.com>
2026-05-28 10:59:26 +0900

    업적 해금 알림창 추가
    업적을 달성하면 캐릭터가 해금되었다고 알리는 알림창을 코루틴을 활용해 추가

--- Assets/Script/AchiveManager.cs
+++ Assets/Script/AchiveManager.cs
@@ -8,6 +8,8 @@ public class AchiveManager : MonoBehaviour
 {
     public GameObject[] lockCharacter;
     public GameObject[] unlockCharacter;
+    //업적해제 알림
+    public GameObject uiNotice;
 
     //업적 종류
     enum Achive { UnlockPotato, UnlockBean }
@@ -15,11 +17,17 @@ public class AchiveManager : MonoBehaviour
     //업적 데이터를 저장
     Achive[] achives;
 
+    //업적 알림에 쓰이는 코루틴 변수
+    WaitForSecondsRealtime wait;
+
     //초기화
     private void Awake()
     {
         //Enum.GetValues : 열거형의 모든 데이터를 가져오는 함수
         achives = (Achive[])Enum.GetValues(typeof(Achive));
+
+        //코루틴 변수 초기화
+        wait = new WaitForSecondsRealtime(5);
 
         //PlayerPrefs에 MyData 값이 0이라면(게임을 처음 실행했다면)
         if (!PlayerPrefs.HasKey("MyData"))
@@ -82,7 +90,8 @@ public class AchiveManager : MonoBehaviour
                 break;
 
             case Achive.UnlockBean:
-                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                //gameTime과 maxGameTime이 float 형이기 때문에 >= 으로 안전하게 비교
+                isAchive = GameManager.instance.gameTime >= GameManager.instance.maxGameTime;
                 break;
         }
 
@@ -91,6 +100,27 @@ public class AchiveManager : MonoBehaviour
         {
             //업적 해금
             PlayerPrefs.SetInt(achive.ToString(), 1);
+            
+            //uiNotice 자식들(UnlockPotato, UnlockBean)을 돌면서
+            for(int index = 0; index < uiNotice.transform.childCount; index++)
+            {
+                //각 업적이 해금됐는지 확인한다음
+                bool isActive = index == (int)achive;
+                //해당 순번의 캐릭터가 해금됐다면 ui 알림 활성화
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            //해금 알림
+            StartCoroutine(NoticeRoutine());
         }
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+        
+        yield return wait;
+
+        uiNotice.SetActive(false);
     }
 }
